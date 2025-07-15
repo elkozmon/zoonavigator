6fdb1d6cdb60f1f87508e75bd5cbd64721df601c
Lubos Kozmon <elkozmon@elkozmon.com>
2025-07-15 23:27:41 +0100

    docs: update theme and config

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'ZooNavigator Docs'
-copyright = u'2021, Lubos Kozmon'
+copyright = u'2025, Lubos Kozmon'
 author = u'Lubos Kozmon'
 
 # The short X.Y version
@@ -58,7 +58,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -175,3 +175,7 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
+# Linkcheck configuration
+linkcheck_ignore = [r'https?://localhost']
+linkcheck_anchors_ignore = [r'issue-\d+']
