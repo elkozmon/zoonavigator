b76ad378068bd8939bb551ff47b3dbaba2ede6eb
Lubos Kozmon <elkozmon@elkozmon.com>
2025-07-18 01:31:12 +0100

    dev: devenv improvements

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,12 @@
 { pkgs, lib, config, inputs, ... }:
 {
   # https://devenv.sh/basics/
-  # env.GREET = "devenv";
+  env.DOCKER_TIMEOUT = "60";
 
   # https://devenv.sh/packages/
-  packages = with pkgs; [ 
+  packages = with pkgs; [
     stdenv
+    coreutils
     colima
 
     # Docker
@@ -22,14 +23,14 @@
   # https://devenv.sh/languages/
   languages = {
     scala.enable = true;
-    
+
     java.jdk.package = pkgs.temurin-jre-bin-17;
-    
+
     javascript = {
       enable = true;
       package = pkgs.nodejs_20;
     };
-    
+
     typescript.enable = true;
 
     python = {
@@ -44,24 +45,64 @@
 
   # https://devenv.sh/processes/
   # avoid mounting $HOME by mounting dummy dir instead (https://github.com/abiosoft/colima/blob/75b104a37eca590e1f72a2cd39ef43ed4093bfef/config/config.go#L134-L143)
-  processes.colima.exec = "colima start --arch x86_64 --memory 4 -f -V /tmp/dummy";
+  processes = {
+    "colima".exec = "colima start --arch x86_64 --memory 4 -f -V /tmp/dummy";
+  };
 
   # https://devenv.sh/services/
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  # scripts.hello.exec = ''
-  #   echo hello from $GREET
-  # '';
+  scripts = {
+    # API
+    "api:run".exec = "cd $DEVENV_ROOT/api && sbt play/run";
+    "api:test".exec = "cd $DEVENV_ROOT/api && sbt test";
+    "api:format".exec = "cd $DEVENV_ROOT/api && sbt scalafmtAll";
+    "api:format:check".exec = "cd $DEVENV_ROOT/api && sbt scalafmtCheckAll";
 
-  # enterShell = ''
-  #   git --version
-  # '';
+    # Web
+    "web:dev".exec = "cd $DEVENV_ROOT/web && npm run dev";
+    "web:build".exec = "cd $DEVENV_ROOT/web && npm run build $@";
+    "web:test".exec = "cd $DEVENV_ROOT/web && npm run test $@";
+    "web:lint".exec = "cd $DEVENV_ROOT/web && npm run lint $@";
+
+    # Docs
+    "docs:build".exec = "cd $DEVENV_ROOT/docs && sphinx-build -W -b html . _build/html";
+    "docs:linkcheck".exec = "cd $DEVENV_ROOT/docs && sphinx-build -b linkcheck . _build/linkcheck";
+  };
+
+  enterShell = ''
+    cat <<EOF
+🚀 Welcome to ZooNavigator Development Environment
+
+Available scripts:
+  📦 API
+    api:run          - Start API server
+    api:test         - Run API tests
+    api:format       - Format API code
+    api:format:check - Check API formatting
+
+  🌐 Web
+    web:dev   - Start web dev server
+    web:build - Build web app
+    web:test  - Run web tests
+    web:lint  - Run web linting
+
+  📚 Docs
+    docs:build     - Build documentation
+    docs:linkcheck - Check documentation links
+
+  🔧 General
+    devenv up   - Start all services
+    devenv test - Run all tests
+
+EOF
+  '';
 
   # https://devenv.sh/tasks/
   tasks = {
     "web:install" = {
-      exec = "cd web && npm install";
+      exec = "cd $DEVENV_ROOT/web && npm install";
       status = "test -d web/node_modules";
       before = [ "devenv:enterShell" ];
     };
@@ -69,8 +110,18 @@
 
   # https://devenv.sh/tests/
   enterTest = ''
-    echo "Running tests"
-    ${pkgs.act}/bin/act \
+    wait_for_processes
+
+    echo "Waiting for Docker"
+    if timeout $DOCKER_TIMEOUT bash -c 'until docker info >/dev/null 2>&1; do sleep 1; done'; then
+      echo "Docker is ready"
+    else
+      echo "Timed out waiting for Docker"
+      exit 1
+    fi
+
+    echo "Starting test workflow"
+    act \
       -W ./.github/workflows/test.yml \
       --artifact-server-path /tmp/actas \
       --container-daemon-socket unix:///var/run/docker.sock
